999e18611f91c251da8eecb16fe5f5cc51b8cb73
Tianye Lu <tianye@corn29.stanford.edu>
2013-03-04 23:04:26 -0800

    updates according to CA's notes, result looks reasonable

--- src/decimate.cpp
+++ src/decimate.cpp
@@ -73,33 +73,6 @@ void simplify(Mesh &mesh, float percentage) {
     std::cout << "Simplifying to #vertices: " << mesh.n_vertices() << std::endl;
 }
 
-void
-update_quadric(Mesh& mesh, Mesh::VertexHandle v_h){
-      quadric(mesh, v_h).clear();
-      for (Mesh::VertexFaceIter vf_it = mesh.vf_iter(v_h); vf_it; ++vf_it) {
-            
-              Mesh::FaceVertexIter fv_it = mesh.fv_iter(vf_it.handle());    
-            Vec3f p0 = mesh.point(fv_it.handle());
-            Vec3f p1 = mesh.point((++fv_it).handle());
-            Vec3f p2 = mesh.point((++fv_it).handle());
-
-            double ax = p0[0], ay = p0[1], az = p0[2];
-            double bx = p1[0], by = p1[1], bz = p1[2];
-            double cx = p2[0], cy = p2[1], cz = p2[2];
-            
-            Vector3d AB(bx-ax, by-ay, bz-az);
-            Vector3d AC(cx-ax, cy-ay, cz-az);
-            Vector3d N = AB.cross(AC);
-            Vector4d q(      
-                  ((by-ay)*(cz-az))-((cy-ay)*(bz-az))
-                , ((bz-az)*(cx-ax))-((cz-az)*(bx-ax))
-                , ((bx-ax)*(cy-ay))-((cx-ax)*(by-ay))
-                , (-1)*((N[0]*ax)+(N[1]*ay)+(N[2]*az))
-              );
-            q.normalize();
-            quadric(mesh, v_h) += Quadricd(q[0], q[1], q[2], q[3]);
-        }
-}
 void initDecimation(Mesh &mesh) {
     // compute face normals
     mesh.update_face_normals();
@@ -112,14 +85,29 @@ void initDecimation(Mesh &mesh) {
 
     for (v_it = mesh.vertices_begin(); v_it != v_end; ++v_it) {
         priority(mesh, v_it) = -1.0;
+        quadric(mesh, v_it.handle()).clear();
         sum = 0;                            // Reset for each iteration
 
         // INSERT CODE HERE FOR PART 1-------------------------------------------------------------------------------
         // calc vertex quadrics from incident triangles
 
-        
-         update_quadric(mesh, v_it.handle());
-
+        for (Mesh::VertexFaceIter vf_it = mesh.vf_iter(v_it); vf_it; ++vf_it) {
+            
+              	Mesh::FaceVertexIter fv_it = mesh.fv_iter(vf_it.handle());    
+            	Vec3f p0 = mesh.point(fv_it.handle());
+            	Vec3f p1 = mesh.point((++fv_it).handle());
+            	Vec3f p2 = mesh.point((++fv_it).handle());
+ 
+            	Vector3d AB(p1[0]-p0[0], p1[1]-p0[1], p1[2]-p0[2]);
+            	Vector3d AC(p2[0]-p0[0], p2[1]-p0[1], p2[2]-p0[2]);
+            	Vector3d N = AB.cross(AC);
+            	
+		Vector4d q(N[0], N[1], N[2], (-1)*((N[0]*p0[0])+(N[1]*p0[1])+(N[2]*p0[2])));
+                q.normalize();
+           
+		quadric(mesh, v_it.handle()) += Quadricd(q[0], q[1], q[2], q[3]);
+        }
+  
         // ----------------------------------------------------------------------------------------------------------
     }
     std::cout << "Finished init" << std::endl;
@@ -185,8 +173,8 @@ float priority(Mesh &mesh, Mesh::HalfedgeHandle _heh) {
    Mesh::VertexHandle v_to_h = mesh.to_vertex_handle(_heh);
    Quadricd Q = quadric(mesh, v_from_h);
    Q += quadric(mesh, v_to_h);
-   
-   return Q((mesh.point(v_to_h)+mesh.point(v_from_h))/2.0);
+  
+   return Q(mesh.point(v_to_h));
 }
 
 void enqueue_vertex(Mesh &mesh, Mesh::VertexHandle _vh) {
@@ -248,17 +236,14 @@ void decimate(Mesh &mesh, unsigned int _n_vertices) {
         hh = target(mesh, from);
         if (is_collapse_legal(mesh, hh)) {
             	to = mesh.to_vertex_handle(hh);
+                quadric(mesh, to) += quadric(mesh, from);
             	mesh.collapse(hh);
-                nv--;
-                update_quadric(mesh, to);
-            	for (vv_it = mesh.vv_iter(to); vv_it; ++vv_it) {
-                update_quadric(mesh, vv_it.handle());       
-            	}
-                
-            	enqueue_vertex(mesh, to);
+            	
+		enqueue_vertex(mesh, to);
          	for(vv_it = mesh.vv_iter(to); vv_it; ++vv_it){
-                 enqueue_vertex(mesh, vv_it.handle());
-         }
+                 	enqueue_vertex(mesh, vv_it.handle());
+         	}
+		nv--;
         }
      }
 
